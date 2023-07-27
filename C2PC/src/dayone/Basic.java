06c3b3a86a411db65bce58826e74bfaca5e79bc2
Susen <Susen@Susenthraprabu>
2023-07-27 10:41:56 +0530

    fdgh

---
+++ C2PC/src/dayone/Basic.java
@@ -0,0 +1,25 @@
+package dayone;
+
+public class Basic {
+
+	public static void main(String[] args) {
+		int a = 5;
+		int f = 7;
+		char b = 'A';
+		float c = 5.9f;
+		double d = 55.9;
+		boolean e = true;
+		int g;
+		g = a+f;
+		
+		System.out.println("value of a is : "+a);
+		System.out.println("Addtion is : "+g);
+		int h = 8;
+		float i =1.8f;
+		float j;
+		j = h+i;
+		System.out.println("value of j is : "+j);
+		
+	}
+
+}
